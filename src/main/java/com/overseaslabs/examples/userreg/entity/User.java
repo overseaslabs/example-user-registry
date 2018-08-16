8c215807c0aa20c2aa88671b508e69701591f4d8
Sergey Vaschenko <vaschenko.sergey@gmail.com>
2018-08-16 10:26:18 +0300

    annotate the user entitiy

--- src/main/java/com/overseaslabs/examples/userreg/entity/User.java
+++ src/main/java/com/overseaslabs/examples/userreg/entity/User.java
@@ -1,17 +1,48 @@
 package com.overseaslabs.examples.userreg.entity;
 
+import java.time.ZonedDateTime;
+
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import javax.persistence.*;
+import javax.validation.constraints.*;
+
+@Entity
+@Table(name = "`user`")
+@EntityListeners(AuditingEntityListener.class)
 public class User {
 
-    private int id;
+    @Id
+    @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "sequence_generator")
+    @SequenceGenerator(name = "sequence_generator", sequenceName = "user_sequence")
+    private Integer id;
+
+    @NotBlank
     private String firstName;
+
+    @NotBlank
     private String lastName;
+
+    @NotBlank
+    @Email
     private String email;
 
-    public int getId() {
+    @CreationTimestamp
+    @Column(name = "created_at", nullable = false, updatable = false)
+    private ZonedDateTime created;
+
+    @UpdateTimestamp
+    @Column(name = "created_at", nullable = false, updatable = false)
+    private ZonedDateTime updated;
+
+
+    public Integer getId() {
         return id;
     }
 
-    public User setId(int id) {
+    public User setId(Integer id) {
         this.id = id;
         return this;
     }
