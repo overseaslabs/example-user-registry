3959f8d840757b73effd41c8500148f769f8e45e
Sergey Vaschenko <vaschenko.sergey@gmail.com>
2018-08-21 10:07:30 +0300

    add beans config

---
+++ src/main/java/com/overseaslabs/examples/ureg/UregConfiguration.java
@@ -0,0 +1,49 @@
+package com.overseaslabs.examples.ureg;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
+import org.springframework.data.redis.connection.RedisStandaloneConfiguration;
+import org.springframework.data.redis.connection.jedis.JedisConnectionFactory;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.listener.ChannelTopic;
+import org.springframework.data.redis.serializer.Jackson2JsonRedisSerializer;
+
+
+@Configuration
+@PropertySource("application.properties")
+public class UregConfiguration {
+
+    @Value("${spring.redis.host}")
+    private String host;
+
+    @Value("${spring.redis.port}")
+    private Integer port;
+
+    @Bean
+    JedisConnectionFactory jedisConnectionFactory() {
+        RedisStandaloneConfiguration config = new RedisStandaloneConfiguration();
+        config.setHostName(host);
+        config.setPort(port);
+        return new JedisConnectionFactory(config);
+    }
+
+    @Bean
+    public RedisTemplate<String, Object> redisTemplate() {
+        final RedisTemplate<String, Object> template = new RedisTemplate<>();
+        template.setConnectionFactory(jedisConnectionFactory());
+        template.setValueSerializer(new Jackson2JsonRedisSerializer<>(Object.class));
+        return template;
+    }
+
+    @Bean
+    ChannelTopic topic() {
+        return new ChannelTopic("example:ureg");
+    }
+
+    @Bean
+    MessagePublisher redisMessagePublisher() {
+        return new RedisMessagePublisher(redisTemplate(), topic());
+    }
+}
